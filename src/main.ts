6900929c7a2476ba84a003b19d76d57d5711c284
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-07-06 23:39:31 +0200

    feat: add feature flags

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { featureFlagsToken } from './app/feature-flags.token';
 import { environment } from './environments/environment';
+
+function loadFeatureFlags(): Promise<{ [feature: string]: boolean }> {
+  return fetch('api/features').then((response) => response.json());
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+loadFeatureFlags()
+  .then((featureFlags) =>
+    platformBrowserDynamic().bootstrapModule(AppModule, {
+      providers: [{ provide: featureFlagsToken, useValue: featureFlags }],
+    })
+  )
+
+  .catch((err) => console.error(err));
