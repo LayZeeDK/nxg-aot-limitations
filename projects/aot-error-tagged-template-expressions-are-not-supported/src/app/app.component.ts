4d698211c3599e2e2bc451595ecc830f60cba29d
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-07-14 16:51:13 +0200

    feat: add working template

--- projects/aot-error-tagged-template-expressions-are-not-supported/src/app/app.component.ts
+++ projects/aot-error-tagged-template-expressions-are-not-supported/src/app/app.component.ts
@@ -3,9 +3,13 @@ import { Component } from '@angular/core';
 const subject = 'World';
 const failingGreeting = String.raw`Hello, ${subject}!`;
 const workingGreeting = `Hello, ${subject}!`;
+const workingTemplate = '<h1>{{ greeting }}</h1>';
 
 @Component({
   selector: 'app-root',
   template: '<h1>' + failingGreeting + '</h1>',
+  // template: workingTemplate,
 })
-export class AppComponent {}
+export class AppComponent {
+  // greeting = String.raw`Hello, ${subject}!`;
+}
